b976effbab1a7a604a918da2b70a1403e05b5fe4
RebeccaLapointe <reblapointe@gmail.com>
2023-01-09 21:09:08 -0500

    Ajout classe partielle immunisation

---
+++ SuiviVaccinDBFirst/Modeles/Immunisation.partial.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuiviVaccinDBFirst.Modeles
+{
+    public partial class Immunisation
+    {
+        public override string ToString()
+        {
+            return $"{ImmunisationId}. Patient : {Nampatient}. " +
+                    $"Date : {Date}. " +
+                    $"Type : {Discriminator} " +
+                    $"({(Discriminator == "Vaccin" ? Vaccin.Nom : Variant)})";
+        }
+    }
+}
--- SuiviVaccinDBFirst/Program.cs
+++ SuiviVaccinDBFirst/Program.cs
@@ -10,17 +10,13 @@ namespace SuiviVaccinDBFirst
             VaccinsContext context = new();
 
             Console.WriteLine("Les types de vaccin.");
-            foreach (Vaccin vaccin in context.Vaccins)
-                Console.WriteLine($"{vaccin.VaccinId}. {vaccin.Nom}");
+            foreach (Vaccin v in context.Vaccins)
+                Console.WriteLine($"{v.VaccinId}. {v.Nom}");
 
             Console.WriteLine();
             Console.WriteLine("Les immunisations.");
-            foreach (Immunisation immunisation in context.Immunisations)
-                Console.WriteLine(
-                    $"{immunisation.ImmunisationId}. Patient : {immunisation.Nampatient}. " +
-                    $"Date : {immunisation.Date}. " +
-                    $"Type : {immunisation.Discriminator} " +
-                    $"({(immunisation.Discriminator == "Vaccin" ? immunisation.Vaccin.Nom : immunisation.Variant)})");
+            foreach (Immunisation i in context.Immunisations)
+                Console.WriteLine(i);
             Console.ReadKey();
         }
     }
